197f4415884babcf15515951731a1447c7bb3eaa
smeghead <smeghead7@gmail.com>
2011-11-20 18:22:34 +0900

    first commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# mudanews #
+
+android app.
+
---
+++ src/com/starbug1/android/nudanews/MudanewsActivity.java
@@ -0,0 +1,13 @@
+package com.starbug1.android.nudanews;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MudanewsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
